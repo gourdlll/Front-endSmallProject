9f771be36d0179b9cf767e3b358e7d87ad882198
gourdlll <1471081146@qq.com>
2019-02-21 17:17:44 +0800

    0221

--- jsStudy/1.main/main.js
+++ jsStudy/1.main/main.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
     'use strict'
+    paper.install(window);//将paper.js声明成全局变量
+    paper.setup(document.getElementById('mainCanvas'));
+    var c = Shape.Circle(200,200,50);//(x.y坐标和半径)
+    c.fillColor = 'green';
+    paper.view.draw();//在屏幕上绘画
+
     console.log('main.jsloaded');
 })
--- jsStudy/READNE.md
+++ jsStudy/READNE.md
@@ -1,2 +1,6 @@
 **`图形库`**
-`直接使用canvas不太方便，所以一般都会借用图形库`
+直接使用canvas不太方便，所以一般都会借用图形库
+例如：Paper.js,KineticJS,Ease1JS,SpriteJS,PixiJS,Fabric.js,Obelisk.js等等
+
+**样板代码**
+使用某个工具类库前的初始化设置
