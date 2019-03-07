384942e854b3a33c805dec50fef898a9c23441c6
gourdlll <1471081146@qq.com>
2019-03-07 16:22:05 +0800

    0307

--- 小程序菜单showActionSheet和选择器picker组合/pages/mature/mature.js
+++ 小程序菜单showActionSheet和选择器picker组合/pages/mature/mature.js
@@ -1,14 +1,24 @@
 // pages/mature/mature.js
 Page({
   data: {
-
+    pickerShow: false,
+    val: [
+      ['e', 'e', 't', 'y']
+    ]
   },
-  
+  finishHandler(e) {
+    console.log(e.detail)
+  },
+
   showActionSheet: function() {
+    var that = this
     wx.showActionSheet({
       itemList: ['A', 'B', 'C'],
       success(res) {
         console.log(res.tapIndex)
+        that.setData({
+          pickerShow: true
+        })
       },
       fail(res) {
         console.log(res.errMsg)
